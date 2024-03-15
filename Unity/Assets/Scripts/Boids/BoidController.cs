1e2882ceb9a632acf405e8e3f873b77bb7dc6748
David Kim <46164193+daehwankim112@users.noreply.github.com>
2024-03-15 09:20:15 -0700

    fix: enable tuning mechanism for boids algorithm

--- Unity/Assets/Scripts/Boids/BoidController.cs
+++ Unity/Assets/Scripts/Boids/BoidController.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class BoidController : MonoBehaviour
 {
-    // [SerializeField] private LineRenderer seperationLine;
-    [SerializeField] private LineRenderer alignmentLine;
+    [SerializeField] private LineRenderer NeighborsLine;
     private int alignmentLineIndex = 0;
     // [SerializeField] private LineRenderer cohesionLine;
 
-    private BoidController[] neighbors;
+    private BoidController[] visualNeighbors;
+    private BoidController[] protectedNeighbors;
     // private List<BoidController> neighbors = new List<BoidController>();
 
     [SerializeField] private Vector3 velocity;
     [SerializeField] private Vector3 acceleration;
 
-    public float maxVelocity = 0.01f;
+    public float fixedVelocity = 0.01f;
     public float separationConstant = 1;
     public float alignmentConstant = 1;
     public float cohesionConstant = 1;
@@ -23,8 +23,8 @@ public class BoidController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        alignmentLine.positionCount = 0;
-        alignmentLine = GetComponent<LineRenderer>();
+        NeighborsLine.positionCount = 0;
+        NeighborsLine = GetComponent<LineRenderer>();
     }
 
     // set methods
@@ -54,7 +54,8 @@ public class BoidController : MonoBehaviour
     public void updateboid()
     {
         velocity += acceleration;
-        velocity = Vector3.ClampMagnitude(velocity, maxVelocity);
+        velocity = velocity.normalized * fixedVelocity;
+        // velocity = Vector3.ClampMagnitude(velocity, maxVelocity);
         
         // keep the position of the boid above the terrain
         this.gameObject.transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + 0.1f, transform.position.z);
@@ -63,27 +64,46 @@ public class BoidController : MonoBehaviour
     }
 
     // update neighboring boids
-    public void checkNeighbors(BoidController[] boidcontrollers, float distance)
+    public void checkNeighbors(BoidController[] boidcontrollers, float visualDistance, float protectedDistance)
     {
-        int neighborsSize = 0;
-        int neighborIndex = 0;
+        int visualNeighborsSize = 0;
+        int visualNeighborIndex = 0;
+        int protectedNeighborSize = 0;
+        int protectedNeighborIndex = 0;
+
+        // count number of neighbors
         foreach (var boid in boidcontrollers)
         {
-            if (boid != this && Vector3.Distance(this.transform.position, boid.gameObject.transform.position) < distance)
+            if (boid != this && Vector3.Distance(this.transform.position, boid.gameObject.transform.position) < visualDistance)
             {
-                neighborsSize++;
+                visualNeighborsSize++;
             }
-        }
-        neighbors = new BoidController[neighborsSize];
-        foreach (var boid in boidcontrollers)
-        {
-            if (boid != this && Vector3.Distance(this.transform.position, boid.gameObject.transform.position) < distance)
+            if (boid != this && Vector3.Distance(this.transform.position, boid.gameObject.transform.position) < protectedDistance)
             {
-                neighbors[neighborIndex] = boid.GetComponent<BoidController>();
-                neighborIndex++;
+                protectedNeighborSize++;
             }
         }
 
+        // initialize neighbors array
+        visualNeighbors = new BoidController[visualNeighborsSize];
+        protectedNeighbors = new BoidController[protectedNeighborSize];
+
+        // populate neighbors array
+        foreach (var boid in boidcontrollers)
+        {
+            if (boid != this && Vector3.Distance(this.transform.position, boid.gameObject.transform.position) < visualDistance)
+            {
+                visualNeighbors[visualNeighborIndex] = boid.GetComponent<BoidController>();
+                visualNeighborIndex++;
+            }
+            if (boid != this && Vector3.Distance(this.transform.position, boid.gameObject.transform.position) < protectedDistance)
+            {
+                protectedNeighbors[protectedNeighborIndex] = boid.GetComponent<BoidController>();
+                protectedNeighborIndex++;
+            }
+        }
+
+        // render lines
         drawLines();
     }
 
@@ -92,13 +112,13 @@ public class BoidController : MonoBehaviour
     public void separation()
     {
         Vector3 steering = Vector3.zero;
-        foreach (var boid in neighbors)
+        foreach (var boid in protectedNeighbors)
         {
             steering += (this.transform.position - boid.transform.position) / Vector3.Magnitude(this.transform.position - boid.transform.position);
         }
-        if (neighbors.Length > 0)
+        if (protectedNeighbors.Length > 0)
         {
-            steering /= neighbors.Length;
+            steering /= protectedNeighbors.Length;
         }
         // steering = Vector3.ClampMagnitude(steering, separationConstant);
         steering *= separationConstant;
@@ -109,13 +129,13 @@ public class BoidController : MonoBehaviour
     public void alignment()
     {
         Vector3 steering = Vector3.zero;
-        foreach (var boid in neighbors)
+        foreach (var boid in visualNeighbors)
         {
             steering += boid.velocity;
         }
-        if (neighbors.Length > 0)
+        if (visualNeighbors.Length > 0)
         {
-            steering /= neighbors.Length;
+            steering /= visualNeighbors.Length;
             steering -= this.velocity;
         }
         // steering = Vector3.ClampMagnitude(steering, alignmentConstant);
@@ -127,13 +147,13 @@ public class BoidController : MonoBehaviour
     public void cohesion()
     {
         Vector3 steering = Vector3.zero;
-        foreach (var boid in neighbors)
+        foreach (var boid in visualNeighbors)
         {
             steering += boid.transform.position;
         }
-        if (neighbors.Length > 0)
+        if (visualNeighbors.Length > 0)
         {
-            steering /= neighbors.Length;
+            steering /= visualNeighbors.Length;
             steering -= this.transform.position;
         }
         // steering = Vector3.ClampMagnitude(steering, cohesionConstant);
@@ -141,20 +161,48 @@ public class BoidController : MonoBehaviour
         this.acceleration += steering;
     }
 
+
     // draw neighboring lines
     public void drawLines()
     {
-        // draw lines for alignment neighbors
-        Vector3[] linePositions = new Vector3[neighbors.Length * 2];
-        for (int i = 0; i < neighbors.Length * 2; i++)
+        // draw lines for visual neighbors
+        Vector3[] linePositions_v = new Vector3[visualNeighbors.Length * 2];
+        for (int i = 0; i < visualNeighbors.Length * 2; i++)
         {
-            linePositions[i] = neighbors[i/2].gameObject.transform.position;
-            linePositions[++i] = this.transform.position;
+            linePositions_v[i] = visualNeighbors[i/2].gameObject.transform.position;
+            linePositions_v[++i] = this.transform.position;
         }
-        alignmentLine.positionCount = neighbors.Length * 2;
-        if (neighbors.Length > 0)
+        NeighborsLine.positionCount = visualNeighbors.Length * 2;
+        if (visualNeighbors.Length > 0)
         {
-            alignmentLine.SetPositions(linePositions);
+            NeighborsLine.SetPositions(linePositions_v);
         }
+
+        /*
+        for (int i = 0; i < protectedNeighbors.Length * 2; i++)
+        {
+            if (protectedNeighbors.Length > 0)
+            {
+                gameObject.GetComponent<Material>().color = Color.red;
+            }
+            protectedNeighbors[i].GetComponent<Material>().color = Color.red;
+        }
+        */
+
+        // draw lines for protected neighbors
+        /*
+        Vector3[] linePositions_p = new Vector3[protectedNeighbors.Length * 2];
+        NeighborsLine.material = NeighborsLine.materials[1];
+        for (int i = 0; i < protectedNeighbors.Length * 2; i++)
+        {
+            linePositions_p[i] = protectedNeighbors[i / 2].gameObject.transform.position;
+            linePositions_p[++i] = this.transform.position;
+        }
+        NeighborsLine.positionCount = protectedNeighbors.Length * 2;
+        if (protectedNeighbors.Length > 0)
+        {
+            NeighborsLine.SetPositions(linePositions_p);
+        }
+        */
     }
 }
--- Unity/Assets/Scripts/Boids/BoidsManager.cs
+++ Unity/Assets/Scripts/Boids/BoidsManager.cs
@@ -11,8 +11,9 @@ public class BoidsManager : MonoBehaviour
     [SerializeField] private float separationConstant = 1;
     [SerializeField] private float alignmentConstant = 1;
     [SerializeField] private float cohesionConstant = 1;
-    [SerializeField] private float neighborDistance = 1f;
-    [SerializeField] private float maxVelocity = 0.01f;
+    [SerializeField] private float visualNeighborDistance = 2f;
+    [SerializeField] private float protectedNeighborDistance = 1f;
+    [SerializeField] private float fixedVelocity = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,14 +24,14 @@ public class BoidsManager : MonoBehaviour
         floak = new BoidController[numberOfBoids];
         for (int i = 0; i < numberOfBoids; i++)
         {
-            var BoidTemp = Instantiate(BoidPrefab, new Vector3(Random.Range(this.transform.position.x - 5f, this.transform.position.x + 5f), 0f, Random.Range(this.transform.position.z - 5f, this.transform.position.z + 5f)), Quaternion.identity);
+            var BoidTemp = Instantiate(BoidPrefab, new Vector3(Random.Range(this.transform.position.x - 10f, this.transform.position.x + 10f), 0f, Random.Range(this.transform.position.z - 10f, this.transform.position.z + 10f)), Quaternion.identity);
             Vector2 tempRandomVector = Random.insideUnitCircle / 100f;
             BoidTemp.gameObject.name = "Boid" + i;
             BoidTemp.GetComponent<BoidController>().setAcceleration(new Vector3(tempRandomVector.x, 0f, tempRandomVector.y));
             BoidTemp.GetComponent<BoidController>().separationConstant = this.separationConstant; 
             BoidTemp.GetComponent<BoidController>().alignmentConstant = this.alignmentConstant; 
             BoidTemp.GetComponent<BoidController>().cohesionConstant = this.cohesionConstant; 
-            BoidTemp.GetComponent<BoidController>().maxVelocity = this.maxVelocity; 
+            BoidTemp.GetComponent<BoidController>().fixedVelocity = this.fixedVelocity; 
             floak[i] = BoidTemp.GetComponent<BoidController>();
         }
     }
@@ -40,7 +41,7 @@ public class BoidsManager : MonoBehaviour
     {
         for (int i = 0; i < numberOfBoids; i++)
         {
-            floak[i].checkNeighbors(floak, neighborDistance);
+            floak[i].checkNeighbors(floak, visualNeighborDistance, protectedNeighborDistance);
             floak[i].alignment();
             floak[i].cohesion();
             floak[i].separation();
